0043f9b18fca2539b1c4fcf95053d6dd8fa88be2
angelica agudelo bautista <angelicaagudelobautista@gmail.com>
2024-06-13 11:43:53 -0500

    primer commit

---
+++ app.js
@@ -0,0 +1,10 @@
+//objeto padreen el navegador
+let win = window;
+let dom = document;
+let head = document.head;
+let body = document.body
+
+console.log(win)
+console.log(dom)
+console.log(head)
+console.log(body)
